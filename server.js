212e057d1767620c26918bd629a8163b46cba658
Matheus de Paula Teixeira <57531203+MatheusTetis@users.noreply.github.com>
2020-04-19 00:54:04 -0300

    Update server.js
    add dynamic GET route

--- server.js
+++ server.js
@@ -18,6 +18,18 @@ app.get('/users', function(req,res){
 })
 
 
+// ou can use dynamic variables to search for specific data associated with an id
+// in your database, and return that (instead of just returning the id).
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
+
+
 // The app.listen method will start up the server locally on the port you 
 // give as its first argument (in this case the base url is: http://localhost:8000)
 app.listen(8000,function(){
