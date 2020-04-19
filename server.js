6a035abd3e255276495b1bc6df1cda9e9e6d0eb1
Matheus de Paula Teixeira <57531203+MatheusTetis@users.noreply.github.com>
2020-04-19 00:39:46 -0300

    Update server.js
    set up express server

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // import Express from node_modules folder
+const app = express(); // creates an instance with the express constructor
+
+
+// The app.listen method will start up the server locally on the port you 
+// give as its first argument (in this case the base url is: http://localhost:8000)
+app.listen(8000,function(){
+console.log("server is running")
+})
